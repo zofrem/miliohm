aac7a7aa26df5e83add7c9c11ca82a30a6da5b5b
Jozef <zofrem@gmail.com>
2020-11-04 20:13:27 +0100

    backlight timer enabled

--- miliohm.ino
+++ miliohm.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal.h>
 #include "LiquidCrystalChart.h"
 #include <inttypes.h>
+#include "LoopTimer.h"
 #include "BarChars.h"
 
 LiquidCrystal* lcd = new LiquidCrystal(12, 11, 5, 4, 3, 2);
@@ -8,15 +9,43 @@ const uint8_t DISPLAY_BARS = 16;
 const uint8_t DISPLAY_ROWS = 2;
 BarChars* barChar =  new BarChars(*lcd);
 LiquidCrystalChart* chartBar = new LiquidCrystalChart(*lcd, *barChar, 0, 0, 3, DISPLAY_BARS);
+LoopTimer* backlightTimer = new LoopTimer(5000);  //10000
+bool backlight = false;
+bool firstRun = true;
 
-void setup() {
-  pinMode(PC13, OUTPUT);
+void setup() 
+{
+  pinMode(PB11, OUTPUT);
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(10);              // wait for a second
-  digitalWrite(PC13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+void loop() 
+{
+  backLightDelayOff();
+  if(firstRun)
+  {
+    activBacklight();
+    firstRun = false;
+  }
+}
+
+void backLightDelayOff()
+{
+  if(backlightTimer->timer())
+  {
+    if(backlight)
+    {
+      backlight = false;
+      digitalWrite(PB11, LOW);
+    }
+  }
+}
+
+void activBacklight()
+{
+  backlightTimer->resetTimer();
+  if(!backlight)
+  {
+    backlight = true;
+    digitalWrite(PB11, HIGH);
+  }
 }
