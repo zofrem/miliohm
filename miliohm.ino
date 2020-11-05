a911a751b070c40eb5c80dbf61ebf5e7773c42e6
Jozef <zofrem@gmail.com>
2020-11-05 20:23:13 +0100

    liquid cristal display connection pinout

--- miliohm.ino
+++ miliohm.ino
@@ -4,18 +4,24 @@
 #include "LoopTimer.h"
 #include "BarChars.h"
 
-LiquidCrystal* lcd = new LiquidCrystal(12, 11, 5, 4, 3, 2);
+LiquidCrystal* lcd = new LiquidCrystal(PB3, PB4, PB6, PB7, PB8, PB9);
 const uint8_t DISPLAY_BARS = 16;
 const uint8_t DISPLAY_ROWS = 2;
 BarChars* barChar =  new BarChars(*lcd);
 LiquidCrystalChart* chartBar = new LiquidCrystalChart(*lcd, *barChar, 0, 0, 3, DISPLAY_BARS);
-LoopTimer* backlightTimer = new LoopTimer(5000);  //10000
+LoopTimer* backlightTimer = new LoopTimer(500000);  //10000
 bool backlight = false;
 bool firstRun = true;
 
 void setup() 
 {
   pinMode(PB11, OUTPUT);
+  lcd->clear();
+  lcd->begin(DISPLAY_BARS, DISPLAY_ROWS);
+  lcd->setCursor(0, 0);
+  lcd->print("miliohm");
+  lcd->setCursor(0, 1);
+  lcd->print("meter");
 }
 
 void loop() 
